9c5f803c1835587d1f1cf9cae3404e56c018589c
gsweq11 <109459448+gsweq11@users.noreply.github.com>
2022-07-24 20:03:14 +0700

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -188,7 +188,6 @@ async def start_comm(client, message: Message, _):
                 )
               
 
-
 @app.on_message(
     filters.command(get_command("START_COMMAND"))
     & filters.group
